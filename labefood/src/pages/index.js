6ff6d2950eee903c14241a3675f9d453719a7df7
RenatoAlexandrini <re.alexandrini@gmail.com>
2022-09-25 21:41:09 -0300

    map dos detalhes do restaurante, sem estilizacao, apenas a logica do map

--- labefood/src/pages/index.js
+++ labefood/src/pages/index.js
@@ -2,7 +2,7 @@ export * from './login';
 export * from './cart';
 export * from './feed';
 export * from './myprofile';
-export * from './search';
+export * from './restaurant-detail';
 export * from './signup';
 export * from './add-address';
 export * from './update-profile';
--- labefood/src/pages/search/index.js
+++
@@ -1,6 +0,0 @@
-export const SearchPage = () =>{
-    return(
-        <h1>Busca</h1>
-    )
-    
-    }
